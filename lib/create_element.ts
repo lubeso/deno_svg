d1ad4477fe37e75509c82847318c555e1653d362
Luis Becerra Solis <lubeso@icloud.com>
2022-12-23 21:42:10 -0500

    Feature: Implement method

--- lib/create_element.ts
+++ lib/create_element.ts
@@ -1,9 +1,56 @@
 import type { ElementConfig } from "/types/element_config.ts";
 import type { ElementReference } from "/types/element_reference.ts";
+import { SVG_NAMESPACE_URI } from "/constants";
 
 /**
  * Create a new immutable SVG element reference.
  */
-export function createElement(_config: ElementConfig): ElementReference {
-  throw new Error("not implemented yet");
+export function createElement(config: ElementConfig): ElementReference {
+  // arrange
+  const { tagName } = config;
+  // act
+  const attributes = parseAttributes(config);
+  const children = parseChildren(config);
+  // return
+  return {
+    tagName,
+    attributes,
+    children,
+  };
+}
+
+/**
+ * Helper function for parsing attribute key-value pairs from an element configuration object.
+ */
+function parseAttributes(
+  config: ElementConfig,
+): ElementReference["attributes"] {
+  // arrange
+  const { tagName } = config;
+  // act
+  const attributes: ElementReference["attributes"] = (tagName === "svg")
+    ? { xmlns: SVG_NAMESPACE_URI }
+    : {};
+  // return
+  return (config.attributes)
+    ? Object.assign(attributes, config.attributes)
+    : attributes;
+}
+
+/**
+ * Helper function for parsing text content or child elements from an element configuration object.
+ */
+function parseChildren(config: ElementConfig): ElementReference["children"] {
+  // arrange
+  const children: ElementReference["children"] = [];
+  // act
+  for (const child of config.children ?? []) {
+    if (typeof child === "string") {
+      children.push(child);
+    } else {
+      children.push(createElement(child));
+    }
+  }
+  // return
+  return children;
 }
