0bf0ebea7e3e2f1e87394d8855b769029626f01c
Luis Becerra Solis <lubeso@icloud.com>
2022-12-23 23:07:58 -0500

    Chore: Implement tests

--- core/to_string_test.ts
+++ core/to_string_test.ts
@@ -1,7 +1,10 @@
 import { describe, it } from "testing/bdd";
-import { assertEquals } from "testing/asserts";
+import { assertStrictEquals } from "testing/asserts";
 import type { ElementReference } from "/types/element_reference.ts";
-import { createElement } from "/core/create_element.ts";
+import { toString } from "/core/to_string.ts";
+
+const TAG_NAME = "text";
+const NEWLINE = "\n";
 
 /** Testing strategy
  * - element has =0 attributes
@@ -12,18 +15,88 @@ import { createElement } from "/core/create_element.ts";
 
 describe("toString()", () => {
   it("element has =0 attributes and =0 children", () => {
-    // TODO
+    // arrange
+    const numAttributes = 0;
+    const numChildren = 0;
+    const ref = generateElement({ numAttributes, numChildren });
+    // act
+    const actual = toString(ref);
+    // assert
+    const expected = `<${TAG_NAME}></${TAG_NAME}`;
+    assertStrictEquals(actual, expected);
   });
   it("element has >0 attributes and =0 children", () => {
-    // TODO
+    // arrange
+    const numAttributes = 2;
+    const numChildren = 0;
+    const ref = generateElement({ numAttributes, numChildren });
+    // act
+    const actual = toString(ref);
+    // assert
+    const expected = join(
+      NEWLINE,
+      `<${TAG_NAME}`,
+      indent(`data-test="0"`, 1),
+      indent(`data-test="1"`, 1),
+      `></${TAG_NAME}>`,
+    );
+    assertStrictEquals(actual, expected);
   });
   it("element has >0 attributes and >0 children", () => {
-    // TODO
+    // arrange
+    const numAttributes = 1;
+    const numChildren = 2;
+    const ref = generateElement({ numAttributes, numChildren });
+    // act
+    const actual = toString(ref);
+    // assert
+    const expected = join(
+      NEWLINE,
+      `<${TAG_NAME}`,
+      indent(`data-test="0"`, 1),
+      `>`,
+      indent("Hello", 1),
+      indent(`<tspan> world!</tspan>`, 1),
+      `</${TAG_NAME}>`,
+    );
+    assertStrictEquals(actual, expected);
   });
   it("element has =0 attributes and >0 children", () => {
-    // TODO
+    // arrange
+    const numAttributes = 0;
+    const numChildren = 1;
+    const ref = generateElement({ numAttributes, numChildren });
+    // act
+    const actual = toString(ref);
+    // assert
+    const expected = join(
+      NEWLINE,
+      `<${TAG_NAME}>`,
+      indent("Hello", 1),
+      `</${TAG_NAME}`,
+    );
+    assertStrictEquals(actual, expected);
   });
 });
+
+/**
+ * Helper function for concatenating a collection of strings.
+ * @param glue string to put in between each piece
+ * @param pieces the strings to join
+ */
+function join(glue: string, ...pieces: string[]): string {
+  return pieces.join(glue);
+}
+
+/**
+ * Helper function for indenting text by a certain amount.
+ * @param text text to indent
+ * @param depth indent level
+ * @param indentation indentation string (defaults to two whitespace characters)
+ */
+function indent(text: string, depth: number, indentation = "  "): string {
+  return `${indentation.repeat(depth)}${text}`;
+}
 
 /**
  * Configuration object for use when generating new element references.
@@ -42,6 +115,6 @@ interface GeneratorOptions {
 /**
  * Helper method for creating new element references.
  */
-function _elementGenerator(_options: GeneratorOptions): ElementReference {
+function generateElement(options: GeneratorOptions): ElementReference {
   throw new Error("not implemented yet");
 }
