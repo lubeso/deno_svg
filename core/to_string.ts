90e57d9e954c18ec5490b743d8ff29df2792fb17
Luis Becerra Solis <lubeso@icloud.com>
2022-12-23 22:46:53 -0500

    Chore: Define new function

---
+++ core/to_string.ts
@@ -0,0 +1,8 @@
+import type { ElementReference } from "/types/element_reference.ts";
+
+/**
+ * Create a valid SVG string representation of the given element.
+ */
+export function toString(_ref: ElementReference): string {
+  throw new Error("not implemented yet");
+}
