1a9888feb71c7a355d90f98058940ca188c1b936
Luis Becerra Solis <lubeso@icloud.com>
2022-12-23 20:58:30 -0500

    Chore: Define testing strategy

---
+++ lib/create_element_test.ts
@@ -0,0 +1,29 @@
+import { describe, it } from "testing/bdd.ts";
+import { assertStrictEquals } from "testing/asserts.ts";
+import { createElement } from "./create_element.ts";
+
+/** Testing strategy
+ * - tag name is 'svg'
+ * - configuration specifies =0 attributes
+ * - configuration specifies >0 attributes
+ * - configuration specifies =0 children
+ * - configuration specifies >0 children
+ */
+
+describe("createElement()", () => {
+  it("tag name is 'svg'", () => {
+    // TODO
+  });
+  it("configuration specifies =0 attributes", () => {
+    // TODO
+  });
+  it("configuration specifies >0 attributes", () => {
+    // TODO
+  });
+  it("configuration specifies =0 children", () => {
+    // TODO
+  });
+  it("configuration specifies >0 children", () => {
+    // TODO
+  });
+});
