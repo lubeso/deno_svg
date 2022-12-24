8fa44637163c48f9cef1550652d4622082236ff5
Luis Becerra Solis <lubeso@icloud.com>
2022-12-23 21:18:17 -0500

    Chore: Implement tests

--- lib/create_element_test.ts
+++ lib/create_element_test.ts
@@ -1,6 +1,9 @@
 import { describe, it } from "testing/bdd.ts";
-import { assertStrictEquals } from "testing/asserts.ts";
+import { assertEquals } from "testing/asserts.ts";
+import type { ElementReference } from "/types/element_reference.ts";
+import type { ElementConfig } from "/types/element_config.ts";
 import { createElement } from "/lib/create_element.ts";
+import { SVG_NAMESPACE_URI } from "/constants";
 
 /** Testing strategy
  * - tag name is 'svg'
@@ -12,18 +15,88 @@ import { createElement } from "/lib/create_element.ts";
 
 describe("createElement()", () => {
   it("tag name is 'svg'", () => {
-    // TODO
+    // arrange
+    const tagName = "svg";
+    // act
+    const actual = createElement({ tagName });
+    // assert
+    const expected: ElementReference = {
+      tagName,
+      attributes: { "xmlns": SVG_NAMESPACE_URI },
+      children: [],
+    };
+    assertEquals(actual, expected);
   });
   it("configuration specifies =0 attributes", () => {
-    // TODO
+    // arrange
+    const tagName = "g";
+    // act
+    const actual = createElement({ tagName });
+    // assert
+    const expected: ElementReference = {
+      tagName,
+      attributes: {},
+      children: [],
+    };
+    assertEquals(actual, expected);
   });
   it("configuration specifies >0 attributes", () => {
-    // TODO
+    // arrange
+    const tagName = "g";
+    const attributes = {
+      "data-test": "test",
+    };
+    // act
+    const actual = createElement({
+      tagName,
+      attributes,
+    });
+    // assert
+    const expected: ElementReference = {
+      tagName,
+      attributes,
+      children: [],
+    };
+    assertEquals(actual, expected);
   });
   it("configuration specifies =0 children", () => {
-    // TODO
+    // arrange
+    const tagName = "g";
+    // act
+    const actual = createElement({ tagName });
+    // assert
+    const expected: ElementReference = {
+      tagName,
+      attributes: {},
+      children: [],
+    };
+    assertEquals(actual, expected);
   });
   it("configuration specifies >0 children", () => {
-    // TODO
+    // arrange
+    const tagName = "g";
+    const children: Array<string | ElementConfig> = [
+      {
+        tagName: "tspan",
+        children: ["Hello"],
+      },
+      "world!",
+    ];
+    // act
+    const actual = createElement({ tagName, children });
+    // assert
+    const expected: ElementReference = {
+      tagName,
+      attributes: {},
+      children: [
+        {
+          tagName: "tspan",
+          attributes: {},
+          children: ["Hello"],
+        },
+        "world!",
+      ],
+    };
+    assertEquals(actual, expected);
   });
 });
