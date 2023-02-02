5cefb997b27c6b5b2ea4b388ec844b404dd1ca86
Luis Becerra Solis <lubeso@icloud.com>
2023-02-01 20:12:58 -0500

    Chore: Clean slate

--- utils/join.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Helper function for concatenating a collection of strings.
- * @param glue string to put in between each piece
- * @param pieces the strings to join
- */
-export function join(glue: string, ...pieces: string[]): string {
-  return pieces.join(glue);
-}
