06cda27f5a1b3484982a62acbc29668f767dc0d7
Luis Becerra Solis <lubeso@icloud.com>
2023-02-02 23:44:51 -0500

    Chore: Implement test cases

---
+++ writer/methods/write_file_test.ts
@@ -0,0 +1,84 @@
+import type { ElementReference } from "../../core/types/element_reference.ts";
+import { createElement } from "../../core/methods/create_element.ts";
+import { toString } from "../../utils/methods/to_string.ts";
+import { writeFile } from "./write_file.ts";
+import { beforeAll, describe, it } from "@std:testing/bdd.ts";
+import { assertEquals } from "@std:testing/asserts.ts";
+
+/** Testing strategy
+ * writeFile():
+ *  - output file contents are correct
+ */
+let directory: string;
+
+describe("writeFile()", () => {
+  beforeAll(async () => {
+    directory = directory ?? await Deno.makeTempDir();
+  });
+  it("output file contents are correct", async () => {
+    // arrange
+    const reference = createElementReference();
+    const filename = crypto.randomUUID();
+    // act
+    await writeFile(reference, { filename, directory });
+    // assert
+    const actual = await Deno.readTextFile(`${directory}/${filename}.svg`);
+    const expected = toString(reference);
+    assertEquals(actual, expected);
+  });
+});
+
+/**
+ * Create a new element definition according to the specified options.
+ */
+function createElementReference(): ElementReference<"svg"> {
+  return createElement({
+    tagName: "svg",
+    attributes: {
+      "width": "100px",
+      "height": "100px",
+    },
+    children: [
+      {
+        tagName: "rect",
+        attributes: {
+          "x": "0%",
+          "y": "0%",
+          "width": "50%",
+          "height": "50%",
+          "fill": "black",
+        },
+      },
+      {
+        tagName: "rect",
+        attributes: {
+          "x": "0%",
+          "y": "50%",
+          "width": "50%",
+          "height": "50%",
+          "fill": "white",
+        },
+      },
+      {
+        tagName: "rect",
+        attributes: {
+          "x": "50%",
+          "y": "0%",
+          "width": "50%",
+          "height": "50%",
+          "fill": "white",
+        },
+      },
+      {
+        tagName: "rect",
+        attributes: {
+          "x": "50%",
+          "y": "50%",
+          "width": "50%",
+          "height": "50%",
+          "fill": "black",
+        },
+      },
+    ],
+  });
+}
