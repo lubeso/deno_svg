84ebac177fc38e4169a83dba91db71619493c1b4
Luis Becerra Solis <lubeso@icloud.com>
2023-02-02 21:23:43 -0500

    Chore: Define testing strategy

---
+++ core/methods/create_element_test.ts
@@ -0,0 +1,8 @@
+/** Testing strategy
+ * createElement():
+ *  - definition specifies no attributes
+ *  - definition specifies some attributes
+ *  - definition specifies no children
+ *  - definition specifies some children
+ *  - definition tag name is "svg"
+ */
