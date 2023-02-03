429a02f8e8f8b809ee8ef49f19c1740b40e707d7
Luis Becerra Solis <lubeso@icloud.com>
2023-02-02 23:16:47 -0500

    Feature: Implement method

--- utils/methods/to_string.ts
+++ utils/methods/to_string.ts
@@ -4,7 +4,83 @@ import type { ElementReference } from "../../core/types/element_reference.ts";
  * Return the given element reference as a string.
  */
 export function toString(
-  _reference: ElementReference<keyof SVGElementTagNameMap>,
+  reference: ElementReference<keyof SVGElementTagNameMap>,
+  currentIndent = 0,
 ): string {
-  throw new Error("not implemented yet");
+  // arrange
+  let str = "";
+  // act
+  str += getOpeningTag(reference, currentIndent);
+  str += getBody(reference, currentIndent);
+  str += getClosingTag(reference);
+  // return
+  return str;
+}
+
+function getOpeningTag(
+  reference: ElementReference<keyof SVGElementTagNameMap>,
+  currentIndent: number,
+): string {
+  // arrange
+  let str = "";
+  // act
+  str += `<${reference.tagName}${getAttributes(reference, currentIndent)}>`;
+  // return
+  return str;
+}
+
+function getAttributes(
+  reference: ElementReference<keyof SVGElementTagNameMap>,
+  currentIndent: number,
+): string {
+  // arrange
+  const entries = Object.entries(reference.attributes);
+  let str = "";
+  // act
+  for (const [key, value] of entries) {
+    str += "\n";
+    str += "  ".repeat(currentIndent + 1);
+    str += `${key}="${value}"`;
+  }
+  if (entries.length > 0) {
+    str += "\n";
+    str += "  ".repeat(currentIndent);
+  }
+  // return
+  return str;
+}
+
+function getBody(
+  reference: ElementReference<keyof SVGElementTagNameMap>,
+  currentIndent: number,
+): string {
+  // arrange
+  let str = "";
+  // act
+  for (const child of reference.children) {
+    str += "\n";
+    str += "  ".repeat(currentIndent + 1);
+    if (typeof child === "string") {
+      str += child;
+    } else {
+      str += toString(child, currentIndent + 1);
+    }
+  }
+  if (reference.children.length > 0) {
+    str += "\n";
+    str += "  ".repeat(currentIndent);
+  }
+  // return
+  return str;
+}
+
+function getClosingTag(
+  reference: ElementReference<keyof SVGElementTagNameMap>,
+): string {
+  // arrange
+  let str = "";
+  // act
+  str += `</${reference.tagName}>`;
+  // return
+  return str;
 }
