9d2cdd11195be539db6c24544b521611fce53a14
Luis Becerra Solis <lubeso@icloud.com>
2023-02-02 22:14:59 -0500

    Chore: Update module exports

--- mod.ts
+++ mod.ts
@@ -1,20 +1,5 @@
 /**
- * Provides types and methods for representing SVG elements.
+ * Provides types and methods for representing SVG document hierarchies.
  */
 
-// Types
-export type { ElementReference } from "./types/element_reference.ts";
-export type { ElementConfig } from "./types/element_config.ts";
-
-// Constants
-import { NAMESPACE_URI } from "./constants/namespace_uri.ts";
-
-// Methods
-import { createElement } from "./core/create_element.ts";
-import { toString } from "./core/to_string.ts";
-
-export default {
-  NAMESPACE_URI,
-  createElement,
-  toString,
-};
+export * from "./core/mod.ts";
