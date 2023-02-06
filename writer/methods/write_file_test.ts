63dfdc3fd78c447fbb0ddf1d28b736efa26353c2
Luis Becerra Solis <lubeso@icloud.com>
2023-02-05 22:57:50 -0500

    Chore: Use fixture in test implementation

--- writer/methods/write_file_test.ts
+++ writer/methods/write_file_test.ts
@@ -1,9 +1,9 @@
 import type { ElementReference } from "../../core/types/element_reference.ts";
 import { createElement } from "../../core/methods/create_element.ts";
-import { toString } from "../../utils/methods/to_string.ts";
 import { writeFile } from "./write_file.ts";
 import { afterAll, beforeAll, describe, it } from "@std:testing/bdd.ts";
 import { assertEquals } from "@std:testing/asserts.ts";
+import { join } from "https://deno.land/std@0.170.0/path/mod.ts";
 
 /** Testing strategy
  * writeFile():
@@ -25,10 +25,19 @@ describe("writeFile()", () => {
     const path = await writeFile(reference, { directory });
     // assert
     const actual = await Deno.readTextFile(path);
-    const expected = toString(reference);
+    const expected = await getFixture("pattern.svg");
     assertEquals(actual, expected);
   });
 });
+
+/**
+ * Return the contents of the fixture with the given name.
+ * @param filename file name and extension
+ */
+async function getFixture(filename: string): Promise<string> {
+  const path = join("fixtures", filename);
+  return await Deno.readTextFile(path);
+}
 
 /**
  * Create a new element definition according to the specified options.
