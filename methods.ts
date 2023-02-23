f1e3897e5ce32dae52244d9fe5bd4a16c3fba364
Luis Becerra Solis <lubeso@icloud.com>
2023-02-23 00:29:38 -0500

    Feature: Implement core method

--- methods.ts
+++ methods.ts
@@ -1,3 +1,4 @@
+import { NAMESPACE_URI } from "./constants.ts";
 import type { ElementDefinition, ElementReference } from "./types.ts";
 
 /**
@@ -6,7 +7,54 @@ import type { ElementDefinition, ElementReference } from "./types.ts";
 export function createElement<
   TagName extends keyof SVGElementTagNameMap = keyof SVGElementTagNameMap,
 >(
-  _definition: ElementDefinition<TagName>,
+  definition: ElementDefinition<TagName>,
 ): ElementReference<TagName> {
-  throw new Error("not implemented yet");
+  // Arrange
+  const { tagName } = definition;
+  // Act
+  const attributes = createAttributes(definition);
+  const children = createChildren(definition);
+  // Return
+  return { tagName, attributes, children };
+}
+
+/**
+ * Create a new attribute key-value mapping according to the given definition.
+ */
+function createAttributes<
+  TagName extends keyof SVGElementTagNameMap = keyof SVGElementTagNameMap,
+>(
+  definition: ElementDefinition<TagName>,
+): ElementReference<TagName>["attributes"] {
+  // Arrange
+  const {
+    tagName,
+    attributes,
+  } = definition;
+  // Act
+  const baseAttributes = tagName === "svg" ? { xmlns: NAMESPACE_URI } : {};
+  // Return
+  return Object.assign(baseAttributes, attributes ?? {});
+}
+
+/**
+ * Create a new list of child elements according to the given definition.
+ */
+function createChildren<
+  TagName extends keyof SVGElementTagNameMap = keyof SVGElementTagNameMap,
+>(
+  definition: ElementDefinition<TagName>,
+): ElementReference<TagName>["children"] {
+  // Arrange
+  const { children } = definition;
+  const _children = [];
+  // Act
+  if (children) {
+    for (const child of children) {
+      const _child = typeof child === "object" ? createElement(child) : child;
+      _children.push(_child);
+    }
+  }
+  // Assert
+  return _children;
 }
