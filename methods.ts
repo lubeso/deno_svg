5eaf7c8d54791c06576171a2c193a033e9c58fa4
Luis Becerra Solis <lubeso@icloud.com>
2023-02-23 00:59:02 -0500

    Chore: Define new methods

--- methods.ts
+++ methods.ts
@@ -58,3 +58,39 @@ function createChildren<
   // Assert
   return _children;
 }
+
+export function toString<
+  TagName extends keyof SVGElementTagNameMap,
+>(_reference: ElementReference): string {
+  throw new Error("not implemented yet");
+}
+
+/**
+ * Options for configuring the `writeFile()` method.
+ */
+export interface WriteFileOptions {
+  /**
+   * Name of the file **without** extension.
+   */
+  name: string;
+  /**
+   * Base path for the new file.
+   */
+  basePath: string;
+}
+
+function getDefaultWriteFileOptions(): WriteFileOptions {
+  const uuid = crypto.randomUUID();
+  const [prefix, ..._] = uuid.split("-");
+  return {
+    name: prefix.toLocaleLowerCase(),
+    basePath: ".",
+  };
+}
+
+export async function writeFile(
+  _reference: ElementReference,
+  _options: WriteFileOptions = getDefaultWriteFileOptions(),
+) {
+  throw new Error("not implemented yet");
+}
