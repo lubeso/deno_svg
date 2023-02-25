6e821eb3f4fb5171c429cb7f25e6bce07d2daa1d
Luis Becerra Solis <lubeso@icloud.com>
2023-02-25 00:09:29 -0500

    Feature: Implement method

--- methods/to_text.ts
+++ methods/to_text.ts
@@ -1,14 +1,18 @@
 import type { ElementReference } from "../types.ts";
+import { indent } from "../utils.ts";
 
 /**
  * Return a valid SVG text representation of the given element.
  */
-export function toText(element: ElementReference): string {
+export function toText(
+  element: ElementReference,
+  currentDepth = 0,
+): string {
   // Arrange
   let text = "";
   // Act
-  text += getOpeningTagText(element);
-  text += getInnerText(element);
+  text += getOpeningTagText(element, currentDepth);
+  text += getInnerText(element, currentDepth);
   text += getClosingTagText(element);
   // Return
   return text;
@@ -17,20 +21,76 @@ export function toText(element: ElementReference): string {
 /**
  * Helper method.
  */
-function getOpeningTagText(_element: ElementReference): string {
-  throw new Error("not implemented yet");
+function getOpeningTagText(
+  element: ElementReference,
+  currentDepth: number,
+): string {
+  // Arrange
+  const { tagName } = element;
+  let text = "";
+  // Act
+  text += indent(
+    `<${tagName}${getAttributesText(element, currentDepth)}>`,
+    currentDepth,
+  );
+
+  // Return
+  return text;
+}
+
+function getAttributesText(
+  element: ElementReference,
+  currentDepth: number,
+): string {
+  // Arrange
+  const { attributes } = element;
+  let text = "";
+  // Act
+  const entries = Object.entries(attributes);
+  for (const [key, value] of entries) {
+    text += "\n";
+    text += indent(`${key}="${value}"`, currentDepth + 1);
+  }
+  if (entries.length > 0) {
+    text += "\n";
+  }
+  // Return
+  return text;
 }
 
 /**
  * Helper method.
  */
-function getInnerText(_element: ElementReference): string {
-  throw new Error("not implemented yet");
+function getInnerText(
+  element: ElementReference,
+  currentDepth: number,
+): string {
+  // Arrange
+  const { children } = element;
+  let text = "";
+  // Act
+  for (const child of children) {
+    text += "\n";
+    text += typeof child === "object"
+      ? toText(element, currentDepth + 1)
+      : indent(child, currentDepth + 1);
+  }
+  if (children.length > 0) {
+    text += "\n";
+  }
+  // Return
+  return text;
 }
 
 /**
  * Helper method.
  */
-function getClosingTagText(_element: ElementReference): string {
-  throw new Error("not implemented yet");
+function getClosingTagText(element: ElementReference): string {
+  // Arrange
+  const { tagName } = element;
+  let text = "";
+  // Act
+  text += `</${tagName}>`;
+  // Return
+  return text;
 }
