cc66c24c852fec078e892f6b7a592823e7ea253a
Luis Becerra Solis <lubeso@icloud.com>
2023-02-24 23:49:51 -0500

    Chore: Define core logic

--- methods/to_text.ts
+++ methods/to_text.ts
@@ -3,6 +3,34 @@ import type { ElementReference } from "../types.ts";
 /**
  * Return a valid SVG text representation of the given element.
  */
-export function toText(_element: ElementReference): string {
+export function toText(element: ElementReference): string {
+  // Arrange
+  let text = "";
+  // Act
+  text += getOpeningTagText(element);
+  text += getInnerText(element);
+  text += getClosingTagText(element);
+  // Return
+  return text;
+}
+
+/**
+ * Helper method.
+ */
+function getOpeningTagText(_element: ElementReference): string {
+  throw new Error("not implemented yet");
+}
+
+/**
+ * Helper method.
+ */
+function getInnerText(_element: ElementReference): string {
+  throw new Error("not implemented yet");
+}
+
+/**
+ * Helper method.
+ */
+function getClosingTagText(_element: ElementReference): string {
   throw new Error("not implemented yet");
 }
